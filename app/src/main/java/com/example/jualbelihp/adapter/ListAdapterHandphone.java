0b9e19646cb837cc2d828b75a5c88820e3b7a02a
Ilhamsutrisna1997 <muhamadilhamsutrisna1997@gmail.com>
2020-04-21 10:21:35 +0700

    Project Individu

---
+++ app/src/main/java/com/example/jualbelihp/adapter/ListAdapterHandphone.java
@@ -0,0 +1,29 @@
+package com.example.jualbelihp.adapter;
+
+import android.content.Context;
+import android.widget.BaseAdapter;
+import android.widget.Filterable;
+
+import java.util.List;
+import java.util.logging.Handler;
+
+import id.my.donny.jualbelihp.model.Handphone;
+
+public class ListAdapterHandphone extends BaseAdapter implements Filterable {
+    private Context context;
+    private List<Handler>list, filterd;
+
+    public ListAdapterHandphone(context, List<Handphone> list){
+        this.context = context;
+        this.list = list;
+        this.filterd = this.list;
+    }
+    @Override
+    public int getCount(){
+        return filterd.size();
+    }
+    @Override
+    
+
+
+}
